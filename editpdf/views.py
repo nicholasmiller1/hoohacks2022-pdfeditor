b0d78aba99250e9c0fcaa2bbc9d2e0678106e0d8
kavya <56983083+kavyarachita@users.noreply.github.com>
2022-03-26 14:12:18 -0400

    wip

--- editpdf/views.py
+++ editpdf/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
 
 
 def index(request):
     return render(request, 'editpdf/base.html')
+
+class EditorView(generic.TemplateView):
+    template_name = 'editpdf/editor.html'
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(**kwargs)
+
+class UploadView(generic.TemplateView):
+    template_name = 'editpdf/upload.html'
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(**kwargs)
+
+class ExportView(generic.TemplateView):
+    template_name = 'editpdf/export.html'
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(**kwargs)
+
+
+
