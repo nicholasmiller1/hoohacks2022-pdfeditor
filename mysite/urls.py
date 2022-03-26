f9a706b3e3c02bddbe5cf187a6bd2209bcdc5f15
kavya <56983083+kavyarachita@users.noreply.github.com>
2022-03-26 14:23:29 -0400

    bootstrap

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,11 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-<<<<<<< HEAD
-from django.shortcuts import HttpResponseRedirect
-=======
 from django.shortcuts import redirect
->>>>>>> main
 
 urlpatterns = [
     path('admin/', admin.site.urls),
