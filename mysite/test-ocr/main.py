b79a8d298017ae4cecb4662b1ebb0faa1a163ca3
John-Zoscak-02 <jzoscak02@gmail.com>
2022-03-27 02:16:18 -0400

    Cleaned up the overlay and pdf wrangling things

--- mysite/test-ocr/main.py
+++ mysite/test-ocr/main.py
@@ -9,7 +9,7 @@ def to_ocr(input_png, output_pdf):
         f.write(ocr)  # pdf type is bytes by default
 
 
-def overlay(input_pdf, output_pdf, output_png, overlay_png):
+def overlay(input_pdf, output_png, overlay_png):
     images = convert_from_path(input_pdf)
     for image in images:
         image.save(output_png)
@@ -19,27 +19,6 @@ def overlay(input_pdf, output_pdf, output_png, overlay_png):
 
     background.paste(foreground, (0, 0), foreground)
     background.save(output_png)
-    
-    # The following code does not work because pdfs cannot have transparency
-    #pdf = PdfFileReader(input_pdf)
-    #pdf_writer = PdfFileWriter()
-    #image_1 = Image.open(overlay_png)
-    #img = image_1.convert('RGB')
-    #img.save('temp.pdf')
-    #overlay = PdfFileReader('temp.pdf')
-    #overlay_page = overlay.getPage(0)
-
-    #pdf_page = pdf.getPage(0)
-    #pdf_page.mergePage(overlay_page)
-    #pdf_writer.addPage(pdf_page)
-
-    #with open(output_pdf, 'wb') as fh:
-    #    pdf_writer.write(fh)
-    
-    #images = convert_from_path(output_pdf)
-    #for image in images:
-    #    image.save(output_png)
-
 
 if __name__ == '__main__':
     overlay(input_pdf='test-2.pdf',
